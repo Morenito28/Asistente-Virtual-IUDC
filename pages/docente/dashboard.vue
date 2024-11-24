ea432846903ec6d19adefdf0e1f65687788b5e22
CristianDCM <cristianzdmc14@gmail.com>
2024-11-24 18:03:43 -0500

    Agregar página de dashboard para docentes con middleware de autenticación

---
+++ pages/docente/dashboard.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  middleware: 'authRole',
+  meta: {
+    role: 'docente',
+  },
+};
+
+</script>
+<template>
+    <div>
+        <h1>Dashboard del Docente</h1>
+        <p>¡Bienvenido!</p>
+    </div>
+</template>
