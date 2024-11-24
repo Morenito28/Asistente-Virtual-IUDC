5679e9eae5cba4384f74449a9b4c628983307fff
CristianDCM <cristianzdmc14@gmail.com>
2024-11-24 18:01:50 -0500

    Agregar configuración de Nuxt para usar nuxt-vuefire

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules:['@nuxtjs/tailwindcss'],
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  runtimeConfig: {
-    public: {
-      firebaseApiKey: process.env.FIREBASE_API_KEY,
-      firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
-      firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
-      firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-      firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-      firebaseAppId: process.env.FIREBASE_APP_ID,
-    },
-  },
-})
-
+  ssr: false,
+  modules: [
+    [
+      "nuxt-vuefire",
+      {
+        auth: true,
+        config: {
+          apiKey: process.env.FIREBASE_API_KEY,
+          authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+          projectId: process.env.FIREBASE_PROJECT_ID,
+          storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+          messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+          appId: process.env.FIREBASE_APP_ID,
+          measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+        },
+      },
+    ],
+    "@nuxtjs/tailwindcss",
+  ],
+});
