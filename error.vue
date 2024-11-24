a325b27e2be1b243820bbda588a5fb710421a8e5
CristianDCM <cristianzdmc14@gmail.com>
2024-11-24 18:01:27 -0500

    Actualizar componente de error para usar un mensaje personalizado en lugar de NuxtError

--- error.vue
+++ error.vue
@@ -1,14 +1,13 @@
 <script setup lang="ts">
-import type { NuxtError } from '#app'
-
-const props = defineProps({
-  error: Object as () => NuxtError
-})
+import { defineProps } from 'vue'
+const props = defineProps<{
+  message: string
+}>()
 </script>
 
 <template>
   <div>
-    <h1>{{ error.statusCode }}</h1>
-    <NuxtLink to="/">Go back home</NuxtLink>
+    <h1>Error</h1>
+    <p>{{ props.message }}</p>
   </div>
 </template>
