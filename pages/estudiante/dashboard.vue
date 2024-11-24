583a36e6735b3d235993b814f9dfb737dee69cf5
CristianDCM <cristianzdmc14@gmail.com>
2024-11-24 18:04:03 -0500

    Agregar página de dashboard para estudiantes con middleware de autenticación

---
+++ pages/estudiante/dashboard.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  middleware: 'authRole',
+  meta: {
+    role: 'estudiante',
+  },
+};
+</script>
+<template>
+    <div>
+        <h1>Dashboard del Estudiante</h1>
+        <p>¡Bienvenido!</p>
+    </div>
+</template>
