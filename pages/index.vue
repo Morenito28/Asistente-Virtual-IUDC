b66b31b3f53dac6900aae5967fb7bb0fc162eb73
CristianDCM <cristianzdmc14@gmail.com>
2024-11-24 18:04:14 -0500

    Eliminar componente TestFirebase de la página de inicio

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,3 @@
 <template>
-  <div>
-    <TestFirebase />
-  </div>
+    <!--  -->
 </template>
-
-<script>
-import TestFirebase from "~/components/TestFirebase.vue";
-
-export default {
-  components: {
-    TestFirebase,
-  },
-};
-</script>
