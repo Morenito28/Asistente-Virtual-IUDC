76c467b6685afc5dfe76d3d2c9d1de1373f4ccd5
s4m.n0v <s4m.n0v@gmail.com>
2024-11-26 20:49:06 +0000

    Flowbite - Tailwind - New proyect

--- app.vue
+++ app.vue
@@ -1,3 +1,15 @@
+<script setup>
+import { onMounted } from 'vue'
+import { useFlowbite } from '~/composables/useFlowbite';
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+    useFlowbite(() => {
+        initFlowbite();
+    })
+})
+</script>
+
 <template>
   <div>
     <NuxtPage />
