2b2003b6c3964a8eceb4934e4f23d55d53299a55
CristianDCM <cristianzdmc14@gmail.com>
2024-11-27 23:40:58 -0500

    Deshabilitar SSR y agregar configuración de Vuefire en nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  ssr: false,
   components: [
     {
       path: '~/components',
@@ -15,7 +16,18 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
-  modules: [
-    '@nuxtjs/tailwindcss'
-  ]
+  modules: ['@nuxtjs/tailwindcss', 'nuxt-vuefire'],
+  vuefire: {
+    auth: {
+      enabled: true,
+    },
+    config: {
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.FIREBASE_APP_ID,
+    }
+  },
 })
