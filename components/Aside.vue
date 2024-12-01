0288c5f8f0db435d642689e9230a65517f887b3f
CristianDCM <cristianzdmc14@gmail.com>
2024-11-30 21:09:34 -0500

    Reemplazar enlaces por router-link en el componente Aside para mejorar la navegación

--- components/Aside.vue
+++ components/Aside.vue
@@ -6,7 +6,7 @@
             <!-- Menu Items -->
             <ul class="space-y-2 font-medium flex-grow">
                 <li v-for="(item, index) in menuItems" :key="index">
-                    <a :href="item.href"
+                    <router-link :to="item.href"
                         class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                         <svg v-if="item.icon"
                             class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
@@ -19,7 +19,7 @@
                             class="inline-flex items-center justify-center px-2 ms-3 text-sm font-medium text-gray-800 bg-gray-100 rounded-full dark:bg-gray-700 dark:text-gray-300">
                             {{ item.badge }}
                         </span>
-                    </a>
+                    </router-link>
                 </li>
             </ul>
 
@@ -46,6 +46,7 @@
         </div>
     </aside>
 </template>
+
 
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
